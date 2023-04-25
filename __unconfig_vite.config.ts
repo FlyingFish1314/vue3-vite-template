f0b9f84f149f8a15d6ace4f1791dba196c3f916e
范恩龙 <1732819972@qq.com>
2023-04-25 21:53:44 +0800

    test(custom): test

---
+++ __unconfig_vite.config.ts
@@ -0,0 +1,65 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
+import vue from '@vitejs/plugin-vue'
+// 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 自动导入样式
+import {
+  createStyleImportPlugin,
+  ElementPlusResolve
+} from 'vite-plugin-style-import'
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.development' })
+} else {
+  dotenv.config({ path: '.env.production' })
+}
+
+// https://vitejs.dev/config/
+const __unconfig_default =  defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+     createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name: string) => {
+            name = name.substring(3, name.length)
+            return `element-plus/es/components/${name}/style/index`
+          }
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    proxy: {
+      // 使用 proxy 实例
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
